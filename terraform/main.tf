7aac95a126bccb2b8bd4ee062137696c2dd3ba1d
Supriyo Mukherjee <supriyomukherjee09@example.com>
2025-01-12 23:51:57 +0530

    Repo Updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "funcapp-blob-store"
+  name                     = "funcappadls"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -16,7 +16,7 @@ resource "azurerm_service_plan" "asp" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type  = "Linux"
-  sku_name = "FREE"
+  sku_name = "S1"
 }
 
 resource "azurerm_linux_function_app" "function_app" {
